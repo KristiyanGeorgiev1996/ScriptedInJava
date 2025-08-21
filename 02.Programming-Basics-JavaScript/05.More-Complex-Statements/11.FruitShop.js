dd0af54863fe637ed06041ebfdfa80ea9edfab9f
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-08-21 19:18:54 +0300

    Add files via upload

---
+++ 02.Programming-Basics-JavaScript/05.More-Complex-Statements/11.FruitShop.js
@@ -0,0 +1,35 @@
+let fruit = prompt("Enter fruit:");
+let weekDay = prompt("Enter day of week:");
+let count = Number(prompt("Enter quantity:"));
+
+let price = 0;
+
+if (weekDay === "Monday" || weekDay === "Tuesday" || weekDay === "Wednesday" || weekDay === "Thursday" || weekDay === "Friday") {
+    switch (fruit) {
+        case "banana": price = count * 2.50; break;
+        case "apple": price = count * 1.20; break;
+        case "orange": price = count * 0.85; break;
+        case "grapefruit": price = count * 1.45; break;
+        case "kiwi": price = count * 2.70; break;
+        case "pineapple": price = count * 5.50; break;
+        case "grapes": price = count * 3.85; break;
+        default: console.log("error"); break;
+    }
+} else if (weekDay === "Saturday" || weekDay === "Sunday") {
+    switch (fruit) {
+        case "banana": price = count * 2.70; break;
+        case "apple": price = count * 1.25; break;
+        case "orange": price = count * 0.90; break;
+        case "grapefruit": price = count * 1.60; break;
+        case "kiwi": price = count * 3.00; break;
+        case "pineapple": price = count * 5.60; break;
+        case "grapes": price = count * 4.20; break;
+        default: console.log("error"); break;
+    }
+} else {
+    console.log("error");
+}
+
+if (price > 0) {
+    console.log(price.toFixed(2));
+}
