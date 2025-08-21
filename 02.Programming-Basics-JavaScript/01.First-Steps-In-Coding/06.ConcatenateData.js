b5c6dc585f204db0aaba4efafe8e8f42cecdfece
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-08-21 19:02:40 +0300

    Add files via upload

---
+++ 02.Programming-Basics-JavaScript/01.First-Steps-In-Coding/06.ConcatenateData.js
@@ -0,0 +1,6 @@
+let firstName = prompt();
+let lastName = prompt();
+let age = Number(prompt());
+let town = prompt();
+
+console.log(`You are ${firstName} ${lastName}, a ${age}-years old person from ${town}.`);
