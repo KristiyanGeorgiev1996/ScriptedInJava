c59a10e466b8cead48380156a5343ad0a2ba99d8
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-08-21 19:02:55 +0300

    Add files via upload

---
+++ 02.Programming-Basics-JavaScript/01.First-Steps-In-Coding/07.ProjectsCreation.js
@@ -0,0 +1,5 @@
+let name = prompt();
+let numberOfProjects = Number(prompt());
+
+let neededHours = numberOfProjects * 3;
+console.log(`The architect ${name} will need ${neededHours} hours to complete ${numberOfProjects} project/s.`);
