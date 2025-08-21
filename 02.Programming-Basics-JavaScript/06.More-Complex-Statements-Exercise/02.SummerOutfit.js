077a05d9eceb1f836543e3acf89abf909f85ad4f
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-08-21 19:20:01 +0300

    Add files via upload

---
+++ 02.Programming-Basics-JavaScript/06.More-Complex-Statements-Exercise/02.SummerOutfit.js
@@ -0,0 +1,51 @@
+// Input
+let degrees = Number(prompt());
+let dayTime = prompt();
+
+// Calculations
+let outfit = "";
+let shoes = "";
+
+if (degrees >= 10 && degrees <= 18) {
+    switch (dayTime) {
+        case "Morning":
+            outfit = "Sweatshirt";
+            shoes = "Sneakers";
+            break;
+        case "Afternoon":
+        case "Evening":
+            outfit = "Shirt";
+            shoes = "Moccasins";
+            break;
+    }
+} else if (degrees > 18 && degrees <= 24) {
+    switch (dayTime) {
+        case "Morning":
+        case "Evening":
+            outfit = "Shirt";
+            shoes = "Moccasins";
+            break;
+        case "Afternoon":
+            outfit = "T-Shirt";
+            shoes = "Sandals";
+            break;
+    }
+} else if (degrees >= 25) {
+    switch (dayTime) {
+        case "Morning":
+            outfit = "T-Shirt";
+            shoes = "Sandals";
+            break;
+        case "Afternoon":
+            outfit = "Swim Suit";
+            shoes = "Barefoot";
+            break;
+        case "Evening":
+            outfit = "Shirt";
+            shoes = "Moccasins";
+            break;
+    }
+}
+
+// Output
+console.log(`It's ${degrees} degrees, get your ${outfit} and ${shoes}.`);
