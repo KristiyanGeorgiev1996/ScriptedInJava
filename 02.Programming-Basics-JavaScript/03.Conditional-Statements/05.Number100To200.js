88001bdf0ecd9e1d353c58d0fd6149f777715b2a
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-08-21 19:12:36 +0300

    Add files via upload

---
+++ 02.Programming-Basics-JavaScript/03.Conditional-Statements/05.Number100To200.js
@@ -0,0 +1,9 @@
+let num = Number(prompt());
+
+if (num < 100) {
+    console.log("Less than 100");
+} else if (num <= 200) {
+    console.log("Between 100 and 200");
+} else if (num > 200) {
+    console.log("Greater than 200");
+}
