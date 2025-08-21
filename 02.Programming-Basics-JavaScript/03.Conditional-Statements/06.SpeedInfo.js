0883a23e3e3bdc984d85a31176926955b50e7250
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-08-21 19:12:51 +0300

    Add files via upload

---
+++ 02.Programming-Basics-JavaScript/03.Conditional-Statements/06.SpeedInfo.js
@@ -0,0 +1,13 @@
+let num = Number(prompt());
+
+if (num <= 10) {
+    console.log("slow");
+} else if (num <= 50) {
+    console.log("average");
+} else if (num <= 150) {
+    console.log("fast");
+} else if (num <= 1000) {
+    console.log("ultra fast");
+} else if (num > 1000) {
+    console.log("extremely fast");
+}
