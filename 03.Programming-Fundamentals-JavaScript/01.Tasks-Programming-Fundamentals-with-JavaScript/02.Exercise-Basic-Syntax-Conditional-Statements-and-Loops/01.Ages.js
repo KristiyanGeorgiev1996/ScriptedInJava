1de9697849a914c5c88c680082b720d732fc983f
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-09-25 17:50:30 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-JavaScript/01.Tasks-Programming-Fundamentals-with-JavaScript/02.Exercise-Basic-Syntax-Conditional-Statements-and-Loops/01.Ages.js
@@ -0,0 +1,13 @@
+let ages = parseInt(prompt());
+
+if (ages >= 0 && ages <= 2) {
+    console.log("baby");
+} else if (ages >= 3 && ages <= 13) {
+    console.log("child");
+} else if (ages >= 14 && ages <= 19) {
+    console.log("teenager");
+} else if (ages >= 20 && ages <= 65) {
+    console.log("adult");
+} else if (ages >= 66) {
+    console.log("elder");
+}
