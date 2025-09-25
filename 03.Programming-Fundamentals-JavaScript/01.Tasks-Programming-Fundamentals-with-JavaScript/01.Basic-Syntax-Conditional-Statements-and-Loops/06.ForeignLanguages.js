9981ca1e3fe2679d03b4fabf22069e35b54fcd65
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-09-25 17:48:18 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-JavaScript/01.Tasks-Programming-Fundamentals-with-JavaScript/01.Basic-Syntax-Conditional-Statements-and-Loops/06.ForeignLanguages.js
@@ -0,0 +1,9 @@
+let country = prompt();
+
+if (country === "USA" || country === "England") {
+    console.log("English");
+} else if (country === "Spain" || country === "Argentina" || country === "Mexico") {
+    console.log("Spanish");
+} else {
+    console.log("unknown");
+}
