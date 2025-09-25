630003ca48ac73676864737b505ab06e499a3568
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-09-25 17:56:21 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-JavaScript/01.Tasks-Programming-Fundamentals-with-JavaScript/04.Exercise-Data-Types-and-Variables/01.IntegerOperations.js
@@ -0,0 +1,10 @@
+let first = parseInt(prompt());
+let sec = parseInt(prompt());
+let third = parseInt(prompt());
+let fourth = parseInt(prompt());
+
+let smetka1 = first + sec;
+let smetka2 = smetka1 / third;
+let smetka3 = smetka2 * fourth;
+
+console.log(smetka3);
