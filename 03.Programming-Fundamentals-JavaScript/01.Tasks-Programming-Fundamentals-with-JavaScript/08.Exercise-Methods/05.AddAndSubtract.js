d321cd89ebab466d14082efd48ebb5a29d1a3fe8
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-09-25 18:50:05 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-JavaScript/01.Tasks-Programming-Fundamentals-with-JavaScript/08.Exercise-Methods/05.AddAndSubtract.js
@@ -0,0 +1,10 @@
+let a = parseInt(prompt());
+let b = parseInt(prompt());
+let c = parseInt(prompt());
+
+function SumOfThreeDigits(a, b, c) {
+    let totalSum = (a + b) - c;
+    console.log(totalSum);
+}
+
+SumOfThreeDigits(a, b, c);
