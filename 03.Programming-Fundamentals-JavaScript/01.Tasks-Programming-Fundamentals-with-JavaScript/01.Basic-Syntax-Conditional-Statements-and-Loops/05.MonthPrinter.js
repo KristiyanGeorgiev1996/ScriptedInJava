3626b68ad0fd3fe4dc9b60d6c76291fa8890bda1
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-09-25 17:48:04 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-JavaScript/01.Tasks-Programming-Fundamentals-with-JavaScript/01.Basic-Syntax-Conditional-Statements-and-Loops/05.MonthPrinter.js
@@ -0,0 +1,29 @@
+let numberOfMonth = parseInt(prompt());
+
+if (numberOfMonth === 1) {
+    console.log("January");
+} else if (numberOfMonth === 2) {
+    console.log("February");
+} else if (numberOfMonth === 3) {
+    console.log("March");
+} else if (numberOfMonth === 4) {
+    console.log("April");
+} else if (numberOfMonth === 5) {
+    console.log("May");
+} else if (numberOfMonth === 6) {
+    console.log("June");
+} else if (numberOfMonth === 7) {
+    console.log("Jule");
+} else if (numberOfMonth === 8) {
+    console.log("August");
+} else if (numberOfMonth === 9) {
+    console.log("September");
+} else if (numberOfMonth === 10) {
+    console.log("October");
+} else if (numberOfMonth === 11) {
+    console.log("November");
+} else if (numberOfMonth === 12) {
+    console.log("December");
+} else {
+    console.log("Error!");
+}
