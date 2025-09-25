5ab63bfa89ae83bde5d0793b08fc487621d1e6af
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-09-25 17:00:31 +0300

    Add files via upload

---
+++ 02.Programming-Basics-JavaScript/10.While-Loop-Exercise/05.Coins.js
@@ -0,0 +1,34 @@
+let inputMoney = parseFloat(prompt());
+
+let change = Math.round(inputMoney * 100);
+let coinsCounter = 0;
+
+while (change > 0) {
+    if (change - 200 >= 0) {
+        change -= 200;
+        coinsCounter++;
+    } else if (change - 100 >= 0) {
+        change -= 100;
+        coinsCounter++;
+    } else if (change - 50 >= 0) {
+        change -= 50;
+        coinsCounter++;
+    } else if (change - 20 >= 0) {
+        change -= 20;
+        coinsCounter++;
+    } else if (change - 10 >= 0) {
+        change -= 10;
+        coinsCounter++;
+    } else if (change - 5 >= 0) {
+        change -= 5;
+        coinsCounter++;
+    } else if (change - 2 >= 0) {
+        change -= 2;
+        coinsCounter++;
+    } else if (change - 1 >= 0) {
+        change -= 1;
+        coinsCounter++;
+    }
+}
+
+console.log(coinsCounter);
