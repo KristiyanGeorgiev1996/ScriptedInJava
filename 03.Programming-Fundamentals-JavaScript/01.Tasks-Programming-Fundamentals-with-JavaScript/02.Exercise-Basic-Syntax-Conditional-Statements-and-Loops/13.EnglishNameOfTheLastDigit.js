f1c6e3f4165bed36e7e27ed740cbe54e31935eb8
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-09-25 17:52:45 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-JavaScript/01.Tasks-Programming-Fundamentals-with-JavaScript/02.Exercise-Basic-Syntax-Conditional-Statements-and-Loops/13.EnglishNameOfTheLastDigit.js
@@ -0,0 +1,25 @@
+let number = parseInt(prompt());
+
+let lastNumber = number % 10;
+
+if (lastNumber === 1) {
+    console.log("one");
+} else if (lastNumber === 2) {
+    console.log("two");
+} else if (lastNumber === 3) {
+    console.log("three");
+} else if (lastNumber === 4) {
+    console.log("four");
+} else if (lastNumber === 5) {
+    console.log("five");
+} else if (lastNumber === 6) {
+    console.log("six");
+} else if (lastNumber === 7) {
+    console.log("seven");
+} else if (lastNumber === 8) {
+    console.log("eight");
+} else if (lastNumber === 9) {
+    console.log("nine");
+} else if (lastNumber === 0) {
+    console.log("zero");
+}
