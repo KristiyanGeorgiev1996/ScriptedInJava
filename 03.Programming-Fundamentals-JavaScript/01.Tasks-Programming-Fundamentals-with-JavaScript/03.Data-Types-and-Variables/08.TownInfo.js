1fcb54612cf13b0a8ff24d4d2cc69b56c8744be8
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-09-25 17:55:07 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-JavaScript/01.Tasks-Programming-Fundamentals-with-JavaScript/03.Data-Types-and-Variables/08.TownInfo.js
@@ -0,0 +1,5 @@
+let town = prompt();
+let population = parseInt(prompt());
+let area = parseInt(prompt());
+
+console.log(`Town ${town} has population of ${population} and area ${area} square km.`);
